be83406b4e9b32530aa69db739eeda24951e9462
Leon <LAnneken09@gmail.com>
2023-04-28 18:42:01 +0200

    First Commit 🎉

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("home-item").addEventListener("click", (ev) => {
+    window.scrollTo({"behavior": "smooth", "top": 0})
+});
+document.getElementById("news-item").addEventListener("click", (ev) => {
+    window.location.href = "#news-seite"
+})
