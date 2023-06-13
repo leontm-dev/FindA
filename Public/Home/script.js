bc4e2dc6b6f80938addbc068d5adfeef3c84a21f
Leon™ <leontm.dev@gmail.com>
2023-06-13 18:17:54 +0200

    New UI

--- Public/Home/script.js
+++ Public/Home/script.js
@@ -1,6 +1,9 @@
 document.getElementById("home-item").addEventListener("click", (ev) => {
-    window.scrollTo({"behavior": "smooth", "top": 0})
+  window.scrollTo({ behavior: "smooth", top: 0 });
 });
 document.getElementById("news-item").addEventListener("click", (ev) => {
-    window.location.href = `${window.location.href}/news`;
+  window.location.href = `${window.location.href}/news`;
+});
+document.getElementById("contact-item").addEventListener("click", (ev) => {
+  window.location.href = "mailto:finda@windthorstgymnasium.de";
 });
--- Routen/API/news.js
+++ Routen/API/news.js
@@ -4,7 +4,7 @@ const express = require("express");
 const dotJson = require("dot-json");
 const env = require("dotenv").config();
 const router = express.Router();
-const fetch = require("@replit/node-fetch")
+const fetch = require("@replit/node-fetch");
 const Utils = require("ltm-utilities");
 
 // Datenbank
@@ -14,36 +14,49 @@ const NEWS = new dotJson("Daten/news.json");
 // Res
 
 const Unauthorized = {
-    message: "Unauthorized",
-    code: 401
+  message: "Unauthorized",
+  code: 401,
 };
 const Success = {
-    message: "Success",
-    code: 200
-}
+  message: "Success",
+  code: 200,
+};
 
 // Routen
 
 router.get("/current", (req, res) => {
-    if (req.headers["finda_key"] === process.env["FINDA_KEY"]) {
-        res.status(200).json(NEWS.get("NEWS"));
-    } else {
-        res.status(401).json(Unauthorized);
-    }
+  if (req.headers["finda_key"] === process.env["FINDA_KEY"]) {
+    res.status(200).json(NEWS.get("NEWS"));
+  } else {
+    res.status(401).json(Unauthorized);
+  }
 });
 router.post("/new/:content", (req, res) => {
-    if (req.headers["finda_key"] === process.env["FINDA_KEY"]) {
-        let news = JSON.parse(req.params.content);
-        let id = new Utils("de-DE").generateId(true, false, false, 10)
-        news.id = id;
-        NEWS.set(`NEWS.${id}`, news);
-        NEWS.save();
-        res.status(200).json(Success);
-    } else {
-        res.status(401).json(Unauthorized);
-    }
+  if (req.headers["finda_key"] === process.env["FINDA_KEY"]) {
+    let news = JSON.parse(req.params.content);
+    let id = new Utils("de-DE").generateId(true, false, false, 10);
+    news.id = id;
+    NEWS.set(`NEWS.${id}`, news);
+    NEWS.save();
+    res.status(200).json(Success);
+  } else {
+    res.status(401).json(Unauthorized);
+  }
 });
-
+router.post("/edit/:id/:content", (req, res) => {
+  if (req.headers["finda_key"] === process.env["FINDA_KEY"]) {
+    if (NEWS.get(`NEWS.${req.params.id}`) != null) {
+      NEWS.set(`NEWS.${req.params.id}`, JSON.parse(req.params.content));
+      NEWS.save();
+      res.status(200);
+    } else {
+      res.status(404);
+    }
+  }
+});
+router.delete("/delete/:id", (req, res) => {
+  NEWS.delete(`NEWS.${req.params.id}`);
+});
 // Exports
 
 module.exports = router;
