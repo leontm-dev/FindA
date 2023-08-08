2787c0e4b57a1673817af58e81199a938a4e741e
Leon™ <leontm.dev@gmail.com>
2023-08-08 19:21:44 +0200

    Removed header

--- Routen/API/news.js
+++ Routen/API/news.js
@@ -13,10 +13,6 @@ const NEWS = new dotJson("Daten/news.json");
 
 // Res
 
-const Unauthorized = {
-  message: "Unauthorized",
-  code: 401,
-};
 const Success = {
   message: "Success",
   code: 200,
@@ -25,33 +21,21 @@ const Success = {
 // Routen
 
 router.get("/current", (req, res) => {
-  if (req.headers["finda_key"] === process.env["FINDA_KEY"]) {
-    res.status(200).json(NEWS.get("NEWS"));
-  } else {
-    res.status(401).json(Unauthorized);
-  }
+  res.status(200).json(NEWS.get("NEWS"));
 });
 router.post("/new/:content", (req, res) => {
-  if (req.headers["finda_key"] === process.env["FINDA_KEY"]) {
-    let news = JSON.parse(req.params.content);
-    let id = new Utils("de-DE").generateId(true, false, false, 10);
-    news.id = id;
-    NEWS.set(`NEWS.${id}`, news);
-    NEWS.save();
-    res.status(200).json(Success);
-  } else {
-    res.status(401).json(Unauthorized);
-  }
+  let news = JSON.parse(req.params.content);
+  let id = new Utils("de-DE").generateId(true, false, false, 10);
+  news.id = id;
+  NEWS.set(`NEWS.${id}`, news);
+  NEWS.save();
+  res.status(200).json(Success);
 });
 router.post("/edit/:id/:content", (req, res) => {
-  if (req.headers["finda_key"] === process.env["FINDA_KEY"]) {
-    if (NEWS.get(`NEWS.${req.params.id}`) != null) {
-      NEWS.set(`NEWS.${req.params.id}`, JSON.parse(req.params.content));
-      NEWS.save();
-      res.status(200);
-    } else {
-      res.status(404);
-    }
+  if (NEWS.get(`NEWS.${req.params.id}`) != null) {
+    NEWS.set(`NEWS.${req.params.id}`, JSON.parse(req.params.content));
+    NEWS.save();
+    res.status(200);
   }
 });
 router.delete("/delete/:id", (req, res) => {
